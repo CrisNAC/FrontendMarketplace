178d45687d1fb3313e0fa6a794777e72b77d5207
LeoAchu <achucarroleonardo@gmail.com>
2026-03-01 19:35:04 -0300

    corregi colores del sidebar de la antigua version

--- src/components/SidebarClientProfile.jsx
+++ src/components/SidebarClientProfile.jsx
@@ -1,11 +1,11 @@
 export const SidebarClientProfile = () => {
      return (
-        <div className="w-full bg-sidebarProfileClient p-2 rounded-md">
+        <div className="w-full bg-[#c0cec2] p-2 rounded-md">
             <ul className="text-[17px] ps-1">
-                <li><a href="" className="block px-2 py-2 rounded hover:bg-sidebarProfileClientHover !text-sidebarProfileClientText font-medium !no-underline">Mi cuenta</a></li>
-                <li><a href="" className="block px-2  py-2 rounded hover:bg-sidebarProfileClientHover !text-sidebarProfileClientText font-medium !no-underline">Mis pedidos</a></li>
-                <li><a href="" className="block px-2 py-2 rounded hover:bg-sidebarProfileClientHover !text-sidebarProfileClientText font-medium !no-underline">Mi lista de favoritos</a></li>
-                <li><a href="" className="block px-2 py-2 rounded hover:bg-sidebarProfileClientHover !text-sidebarProfileClientText font-medium !no-underline">Libreta de direcciones</a></li>
+                <li><a href="" className="block px-2 py-2 rounded hover:bg-[#9aa89c] !text-[#2d4030] font-medium !no-underline">Mi cuenta</a></li>
+                <li><a href="" className="block px-2  py-2 rounded hover:bg-[#9aa89c] !text-[#2d4030] font-medium !no-underline">Mis pedidos</a></li>
+                <li><a href="" className="block px-2 py-2 rounded hover:bg-[#9aa89c] !text-[#2d4030] font-medium !no-underline">Mi lista de favoritos</a></li>
+                <li><a href="" className="block px-2 py-2 rounded hover:bg-[#9aa89c] !text-[#2d4030] font-medium !no-underline">Libreta de direcciones</a></li>
             </ul>
         </div>
     );
