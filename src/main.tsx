ed18fd44b2d06b2068df7b93330b322290e5a452
José Díaz <jose.diaz@LOCAL.ITAPUA.GOV.PY>
2026-03-01 17:36:46 -0300

    OM-153: Migración a la versión 4 de tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css"; // NO TOCAR (KISSER SE LE VA CAER EL PELO)
 import './index.css';
 import App from './App.tsx';
-import "bootstrap/dist/css/bootstrap.min.css"; //NO TOCAR (KISSER SE LE VA CAER EL PELO)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
