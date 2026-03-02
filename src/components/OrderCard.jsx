b673e4ae81e3c42da4a2d9728a7359fe2fadb174
LeoAchu <achucarroleonardo@gmail.com>
2026-03-02 14:58:54 -0300

    OM-9: ajuste margenes en OrderCard.jsx

--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -31,20 +31,20 @@ const iconosEstado = {
 
 export const OrderCard = ({ order, onClick }) => {
   return (
-    // Añadí un gap-4 general para asegurar espacio entre las secciones
-    <div onClick={onClick} className="flex justify-between items-center border border-gray-200 rounded-lg p-4 mb-4 bg-white shadow-sm gap-4 cursor-pointer hover:bg-slate-50 hover:border-blue-400 hover:shadow-md transition-all duration-200">
+    
+    <div onClick={onClick} className="flex justify-between items-center border border-gray-200 rounded-lg p-1 mb-3 bg-white shadow-sm gap-4 cursor-pointer hover:bg-slate-50 hover:border-blue-400 hover:shadow-md transition-all duration-200">
       
       {/* Agrupamos el icono con los textos a la izquierda */}
-      <div className="flex items-center gap-4">
+      <div className="flex items-center ms-2 gap-4">
         
-        {/* Aquí se renderiza el icono del estado del pedido (con un fallback por si el estado viene mal) */}
+        {/* Aca se renderiza el icono del estado del pedido */}
         {iconosEstado[order.estado] || iconosEstado["Pendiente"]}
 
         {/* id y fecha del pedido*/}
         <div>
-          <h4 className="text-base font-semibold mb-1">
+          <p className="text-base font-semibold mb-1">
             {order.id}
-          </h4>
+          </p>
           <p className="text-gray-500 text-sm">
             {order.fecha}
           </p>
@@ -57,7 +57,7 @@ export const OrderCard = ({ order, onClick }) => {
       </div>
 
       {/* Cantidad del producto y el total */}
-      <div className="text-right shrink-0">
+      <div className="text-right me-2 shrink-0">
         <p className="text-sm mb-1 text-gray-600">{order.cantidad} producto(s)</p>
         <p className="text-lg font-bold">${order.total}</p>
       </div>
