dd603dde8850024bc95a387e341fea7051760316
Benjakr04 <benjaminkravchuk04@gmail.com>
2026-03-12 15:19:23 -0300

    OM-236 agregue productDetailApi.js

---
+++ src/features/commerces/services/productDetailApi.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+
+export const getProductById = async (id) => {
+
+const response = await axios.get(`${API_BASE_URL}/products/${id}`);
+
+return response.data;
+
+};
