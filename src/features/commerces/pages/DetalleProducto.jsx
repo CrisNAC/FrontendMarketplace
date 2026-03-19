9da8c91cd1ed4040b5d343ac7def7de05038af6d
LeoAchu <achucarroleonardo@gmail.com>
2026-03-18 21:10:43 -0300

    OM-122: ahora se pasa id del producto como parametro a la ruta de comentarios en DetalleProducto.js

--- src/features/commerces/pages/DetalleProducto.jsx
+++ src/features/commerces/pages/DetalleProducto.jsx
@@ -330,7 +330,7 @@ export default function DetalleProducto() {
 
           <button
             type="button"
-            onClick={() => navigate("/comentarios")}
+            onClick={() => navigate(`/comentarios/${productId}`)}
             className="text-[18px] font-semibold text-black border-b border-black pb-[2px]"
           >
             <h2>Comentarios</h2>
