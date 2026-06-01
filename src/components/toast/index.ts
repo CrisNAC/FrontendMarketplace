6514110171578c9db590eb151abb374098ec8fea
CrisNAC <cristian.acuna@fiuni.edu.py>
2026-06-01 20:27:30 -0300

    OM-538: Add new index files for toast, commerces, hooks, and layouts components

---
+++ src/components/toast/index.ts
@@ -0,0 +1,4 @@
+export { ToastContainer } from "./ToastContainer";
+export { ToastProvider } from "./ToastProvider";
+export { ToastContext } from "./ToastContext";
+export type { ToastType, Toast, ToastContextType } from "./ToastTypes";
---
+++ src/features/commerces/hooks/index.js
@@ -0,0 +1,4 @@
+export { useCategoryRequest } from "./useCategoryRequest";
+export { MAX_TAGS, MAX_VISIBLE_TAG_SUGGESTIONS, useCreateProduct } from "./useCreateProduct";
+export { useEditProduct } from "./useEditProduct";
+export { useEditCommerce } from "./useEditCommerce";
